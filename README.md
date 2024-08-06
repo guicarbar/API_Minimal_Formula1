1ca5fc32203e514e9b743fda645152f30d4d2a99
guicarbar <guicarbar@gmail.com>
2024-08-06 20:49:50 -0300

    update reademe

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
+🟢 🟡 🔴
+
 # API_Minimal_Formula1
- 
+
+:car: :car:
+
+## Sobre
+
+Este é um projeto de uma mini API utilizando Node.js e o framework Justify. Esta API é destinada a apresentar as equipes de Fórmula 1 e todos os pilotos que participaram da temporada de 2024. Além disso, a API oferece a funcionalidade de filtrar os pilotos com base em seus respectivos IDs.
+
+## Tecnologias :gem:
+
+![NodeJS](https://img.shields.io/badge/node.js-6DA55F?style=for-the-badge&logo=node.js&logoColor=white)
+![TypeScript](https://img.shields.io/badge/typescript-%23007ACC.svg?style=for-the-badge&logo=typescript&logoColor=white)
+
+
+:memo:
--- code/src/server.ts
+++ code/src/server.ts
@@ -11,16 +11,36 @@ const teams = [
     {id: 1, name: "Ferrari"},
     {id: 2, name: "McLaren"},
     {id: 3, name: "Mercedes"},
-    {id: 4, name: "Red Bull Racing"}
-]
+    {id: 4, name: "Red Bull Racing"},
+    {id: 5, name: "Alpine"},
+    {id: 6, name: "Aston Martin"},
+    {id: 7, name: "AlphaTauri"},
+    {id: 8, name: "Alfa Romeo"},
+    {id: 9, name: "Haas"},
+    {id: 10, name: "Williams"}
+];
 
 const drivers = [
     {id: 1, name: "Max Verstappen", team: "Red Bull Racing"},
     {id: 2, name: "Lando Norris", team: "McLaren"},
     {id: 3, name: "Charles Leclerc", team: "Ferrari"},
     {id: 4, name: "Lewis Hamilton", team: "Mercedes"},
-    {id: 5, name: "Sergio Perez", team: "Red Bull Racing"}
-]
+    {id: 5, name: "Sergio Perez", team: "Red Bull Racing"},
+    {id: 6, name: "Carlos Sainz", team: "Ferrari"},
+    {id: 7, name: "George Russell", team: "Mercedes"},
+    {id: 8, name: "Daniel Ricciardo", team: "AlphaTauri"},
+    {id: 9, name: "Yuki Tsunoda", team: "AlphaTauri"},
+    {id: 10, name: "Fernando Alonso", team: "Aston Martin"},
+    {id: 11, name: "Lance Stroll", team: "Aston Martin"},
+    {id: 12, name: "Esteban Ocon", team: "Alpine"},
+    {id: 13, name: "Pierre Gasly", team: "Alpine"},
+    {id: 14, name: "Valtteri Bottas", team: "Alfa Romeo"},
+    {id: 15, name: "Guanyu Zhou", team: "Alfa Romeo"},
+    {id: 16, name: "Kevin Magnussen", team: "Haas"},
+    {id: 17, name: "Nico Hülkenberg", team: "Haas"},
+    {id: 18, name: "Alex Albon", team: "Williams"},
+    {id: 19, name: "Logan Sargeant", team: "Williams"}
+];
 
 server.get("/teams", async(request, response) => {
     response.type("application/json").code(200)
